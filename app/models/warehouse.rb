df912dd8b272f9c7377c1e91a636319813a245a3
Sade Oliveira <sade.oli95@gmail.com>
2022-09-15 21:57:28 -0300

    08 - Listando Galpões Parte 1: ok

---
+++ app/models/warehouse.rb
@@ -0,0 +1,4 @@
+class Warehouse < ApplicationRecord
+
+    
+end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -5,10 +5,29 @@ describe 'Usuario visita tela inicial' do
         # Arrange
 
         # Act
-        visit('/')
+        visit root_path
 
         # Assert
         expect(page).to have_content('Galpões & Estoque')
-
     end
+
+    it 'e vê os galpões cadastrados' do
+        # Arrange
+        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+        Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+
+        # Act
+        visit root_path
+
+        # Assert
+        expect(page).to have_content('Rio')
+        expect(page).to have_content('Código: SDU')
+        expect(page).to have_content('Cidade: Rio de Janeiro')
+        expect(page).to have_content('60000 m2')
+
+        expect(page).to have_content('Maceio')
+        expect(page).to have_content('Código: MCZ')
+        expect(page).to have_content('50000')
+    end
+
 end
