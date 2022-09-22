6ed397f54cd10dde95076c76070cf82cb463c244
Sade Oliveira <sade.oli95@gmail.com>
2022-09-21 22:20:57 -0300

    17 - Testes unitários: ok

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,108 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+    describe '#valid?' do
+        context 'presence' do
+            it  'false when name is empty' do
+            # Arrange
+            warehouse = Warehouse.new(name: '', code: 'SDU', city: 'Rio de Janeiro', area: 60000, 
+                            description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', cep: '20000-000', state: 'RJ')
+            
+            # Act
+            result = warehouse.valid?
+
+            # Assert
+            expect(result).to eq false
+            end
+
+            it  'false when code is empty' do
+                # Arrange
+                warehouse = Warehouse.new(name: 'Rio', code: '', city: 'Rio de Janeiro', area: 60000, 
+                                description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', cep: '20000-000', state: 'RJ')
+                
+                # Act
+                result = warehouse.valid?
+        
+                # Assert
+                expect(result).to eq false
+            end
+
+            it  'false when city is empty' do
+                # Arrange
+                warehouse = Warehouse.new(name: 'Rio', code: 'SDU', city: '', area: 60000, 
+                                description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', cep: '20000-000', state: 'RJ')
+                
+                # Act
+                result = warehouse.valid?
+        
+                # Assert
+                expect(result).to eq false
+            end
+
+            it  'false when area is empty' do
+                # Arrange
+                warehouse = Warehouse.new(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: '', 
+                                description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', cep: '20000-000', state: 'RJ')
+                
+                # Act
+                result = warehouse.valid?
+        
+                # Assert
+                expect(result).to eq false
+            end
+        end
+
+        context 'uniqueness' do
+            it 'false when code is lready in use' do 
+                # Arrange
+                first_warehouse = Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60000, 
+                                                    description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', 
+                                                    cep: '20000-000', state: 'RJ')
+
+                second_warehouse = Warehouse.new(name: 'Niteroi', code: 'SDU', city: 'Niteroi', area: 65000, 
+                                                description: 'Galpão de Niteroi', adress: 'Av. do Porto, 50',
+                                                cep: '30000-000', state: 'RJ')
+
+                # Act
+                result = second_warehouse.valid?
+
+                # Assert
+                expect(result).to eq false
+            end
+
+            it 'false when cep is lready in use' do 
+                # Arrange
+                first_warehouse = Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60000, 
+                                                    description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', 
+                                                    cep: '20000-000', state: 'RJ')
+
+                second_warehouse = Warehouse.new(name: 'Niteroi', code: 'NIT', city: 'Niteroi', area: 65000, 
+                                                description: 'Galpão de Niteroi', adress: 'Av. do Porto, 50',
+                                                cep: '20000-000', state: 'RJ')
+
+                # Act
+                result = second_warehouse.valid?
+
+                # Assert
+                expect(result).to eq false
+            end
+        end
+
+        context 'format' do
+            it 'false when cep is not 00000-000' do 
+                # Arrange
+                warehouse = Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60000, 
+                                                    description: 'Galpão do Rio', adress: 'Av. do Porto, 1000', 
+                                                    cep: '20000000', state: 'RJ')
+
+                # Act
+                result = warehouse.valid?
+
+                # Assert
+                expect(result).to eq false
+            end
+        end
+    end
+
+
 end
